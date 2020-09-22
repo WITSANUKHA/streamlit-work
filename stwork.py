f996fd0fc882882c70d6c02e430f6ed4b9d60280
WITSANUKHA <69754065+WITSANUKHA@users.noreply.github.com>
2020-09-22 16:37:19 +0700

    Update stwork.py

--- stwork.py
+++ stwork.py
@@ -93,3 +93,35 @@ st.altair_chart(alt.Chart(chart_data)
 if st.checkbox("Show raw data", False):
     st.subheader("Raw data by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
     st.write(data)
+
+######################
+
+crs = "EPSG:4326"
+geometry = gp.points_from_xy(df.lon,df.lat)
+geo_df  = gp.GeoDataFrame(df,crs=crs,geometry=geometry)
+
+nan_boundary  = gp.read_file('https://github.com/Maplub/AirQualityData/blob/master/nan_shp_wgs84.zip?raw=true')
+nanall = nan_boundary.unary_union
+
+nan_sta = geo_df.loc[geo_df.geometry.within(nanall)]
+
+
+longitude = 100.819200
+latitude = 19.331900
+
+station_map = fo.Map(
+	location = [latitude, longitude], 
+	zoom_start = 10)
+
+latitudes = list(nan_sta.lat)
+longitudes = list(nan_sta.lon)
+labels = list(nan_sta.name)
+
+for lat, lng, label in zip(latitudes, longitudes, labels):
+	fo.Marker(
+		location = [lat, lng], 
+		popup = label,
+		icon = fo.Icon(color='red', icon='heart')
+	).add_to(station_map)
+
+folium_static(station_map)
