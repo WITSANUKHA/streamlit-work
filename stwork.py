9f1169a24942752f6d68b6796cf45a41025e0e71
WITSANUKHA <69754065+WITSANUKHA@users.noreply.github.com>
2020-09-22 16:15:00 +0700

    Update stwork.py

--- stwork.py
+++ stwork.py
@@ -5,7 +5,6 @@ import folium as fo
 from streamlit_folium import folium_static
 import geopandas as gp
 import datetime
-
 import altair as alt
 import pydeck as pdk
 
@@ -21,31 +20,31 @@ Mr.WITSANU KHAKHRUANGRUAN 6030822121
 ## [See my source code](https://github.com/WITSANUKHA/streamlit-work/blob/master/stwork.py)
 '''
 
-START = "timestart"
+start = "timestart"
 day = st.slider("Select Day",1,5,step = 1)
 if day == 1:
-        DATA_URL = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190101.csv")
+        url = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190101.csv")
 elif day == 2:
-        DATA_URL = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190102.csv")
+        url = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190102.csv")
 elif day == 3:
-        DATA_URL = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190103.csv")
+        url = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190103.csv")
 elif day == 4:
-        DATA_URL = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190104.csv")
+        url = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190104.csv")
 else:
-        DATA_URL = ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190105.csv")
+        url= ("https://raw.githubusercontent.com/WITSANUKHA/streamlit-work/master/20190105.csv")
 
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(url, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis="columns", inplace=True)
-    data[START] = pd.to_datetime(data[START], format = '%d/%m/%Y %H:%M')
+    data[start] = pd.to_datetime(data[start], format = '%d/%m/%Y %H:%M')
     return data
 
 data = load_data(100000)
 
 hour = st.slider("Hour to look at", 0, 23, step = 3)
 
-data = data[data[START].dt.hour == hour]
+data = data[data[start].dt.hour == hour]
 data = data.drop(data.columns[0], axis=1)
 for i in range(5):
     data = data.drop(data.columns[6], axis=1)
@@ -77,9 +76,9 @@ st.write(pdk.Deck(
 
 st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
 filtered = data[
-    (data[START].dt.hour >= hour) & (data[START].dt.hour < (hour + 3))
+    (data[start].dt.hour >= hour) & (data[start].dt.hour < (hour + 3))
 ]
-hist = np.histogram(filtered[START].dt.minute, bins=60, range=(0, 60))[0]
+hist = np.histogram(filtered[start].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
 
 st.altair_chart(alt.Chart(chart_data)
