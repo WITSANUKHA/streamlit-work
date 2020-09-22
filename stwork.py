d083f3a29d3cf2ce1fd60ab6614a05d14049cb63
WITSANUKHA <69754065+WITSANUKHA@users.noreply.github.com>
2020-09-22 17:15:05 +0700

    Update stwork.py

--- stwork.py
+++ stwork.py
@@ -40,14 +40,14 @@ def load_data(nrows):
 
 data = load_data(100000)
 
-hour = st.slider("Hour to look at", 0, 23)
+hour = st.slider("Select Hour", 0, 23)
 
 data = data[data[start].dt.hour == hour]
 data = data.drop(data.columns[0], axis=1)
 for i in range(5):
     data = data.drop(data.columns[6], axis=1)
 
-st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+st.subheader("Data between %i:00 and %i:00" % (hour, (hour + 3) % 24))
 midpoint = (np.average(data["latstartl"]), np.average(data["lonstartl"]))
 
 st.write(pdk.Deck(
@@ -71,13 +71,30 @@ st.write(pdk.Deck(
         ),
     ],
 ))
-
-st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+st.altair_chart(alt.Chart(chart_data)
+    .mark_area(
+        interpolate='step-after',
+    ).encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=['minute', 'pickups']
+    ), use_container_width=True)
+st.subheader("By minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
 filtered = data[
     (data[start].dt.hour >= hour) & (data[start].dt.hour < (hour + 3))
 ]
 hist = np.histogram(filtered[start].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
+
+st.altair_chart(alt.Chart(chart_data)
+    .mark_area(
+        interpolate='step-after',
+    ).encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=['minute', 'pickups']
+        color=alt.value('red')
+    ), use_container_width=True)
 
 if st.checkbox("Show raw data", False):
     st.subheader("Raw data by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
